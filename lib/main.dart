2347af2280008e48ec084e4ec199a56e9854a5ad
Muhammad Ahmad Bilal <ahmed.bilal575@gmail.com>
2022-08-02 10:23:35 +0500

    Added Sqlite for the database

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,65 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:logging/logging.dart';
 import 'package:provider/provider.dart';
 
-import '../data/message_dao.dart';
-import 'data/user_dao.dart';
-import 'ui/login.dart';
-import 'ui/message_list.dart';
+// import 'data/memory_repository.dart';
+// import 'data/sqlite/sqlite_repository.dart';
+import 'data/moor/moor_repository.dart';
+import 'data/repository.dart';
+import 'network/recipe_service.dart';
+import 'network/service_interface.dart';
+import 'ui/main_screen.dart';
 
 Future<void> main() async {
+  _setupLogging();
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const App());
+  // final repository = SqliteRepository();
+  final repository = MoorRepository();
+  await repository.init();
+  runApp(MyApp(repository: repository));
 }
 
-class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
+void _setupLogging() {
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen(
+    (rec) {
+      print('${rec.level.name}: ${rec.time}: ${rec.message}');
+    },
+  );
+}
 
+class MyApp extends StatelessWidget {
+  final Repository repository;
+
+  const MyApp({Key? key, required this.repository}) : super(key: key);
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider<UserDao>(
+        Provider<Repository>(
           lazy: false,
-          create: (_) => UserDao(),
+          create: (_) => repository,
+          dispose: (_, Repository repository) => repository.close(),
         ),
-        Provider<MessageDao>(
+        Provider<ServiceInterface>(
+          // create: (_) => MockService()..create(),
+          create: (_) => RecipeService.create(),
           lazy: false,
-          create: (_) => MessageDao(),
         ),
       ],
       child: MaterialApp(
+        title: 'Recipes',
         debugShowCheckedModeBanner: false,
-        title: 'RayChat',
         theme: ThemeData(
-          primaryColor: const Color(0xFF3D814A),
+          brightness: Brightness.light,
+          primaryColor: Colors.white,
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        home: Consumer<UserDao>(
-          builder: (context, userDao, child) {
-            if (userDao.isLoggedIn()) {
-              return const MessageList();
-            } else {
-              return const Login();
-            }
-          },
-        ),
+        home: const MainScreen(),
       ),
     );
   }
